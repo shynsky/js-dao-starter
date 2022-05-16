c2b6bba48f600278be82eaa54d66f053dbc7dc0f
marek.wituszynski@gmail.com <marek.wituszynski@gmail.com>
2022-05-16 09:58:35 +0200

    DAO namechange (silly mistake?)

--- src/App.jsx
+++ src/App.jsx
@@ -175,7 +175,7 @@ const App = () => {
   if (!address) {
     return (
       <div className='landing'>
-        <h1>Welcome to NarutoDAO</h1>
+        <h1>Welcome to ManiaDAO</h1>
         <button onClick={connectWithMetamask} className='btn-hero'>
           Connect your wallet
         </button>
